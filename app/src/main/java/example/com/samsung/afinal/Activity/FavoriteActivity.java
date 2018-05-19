7ea53eb1029a57af9195777cc75851c38ae17128
Unknown <runabird36@gmail.com>
2018-05-19 21:52:13 +0900

    add preference fragment and adjust other
    adjust favorite activity into favorite fragment and then main view into main fragment andthen three fragment change each other

--- app/src/main/java/example/com/samsung/afinal/Activity/FavoriteActivity.java
+++ app/src/main/java/example/com/samsung/afinal/Activity/FavoriteActivity.java
@@ -4,9 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.ContextMenu;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
@@ -31,7 +28,7 @@ public class FavoriteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorite);
+        setContentView(R.layout.fragment_favorite);
 
         plusButton = findViewById(R.id.plusButton);
 
